e898eeaa3cff9fa154b8e674d23a127e17ad18f9
sourceCodeISA <isabel.gogov1@libero.it>
2020-12-09 16:01:43 +0100

    commenti

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+var appH = new Vue({
+  el: '#app-header',
+  data: {
+    // header link
+  },
+  methods: {
+    
+   
+    
+    
+  }
+
+});
+
+var appF = new Vue({
+  el: '#app-footer',
+  data: {
+    // footer link
+  },
+  methods: {
+    
+   
+    
+    
+  }
+
+});
